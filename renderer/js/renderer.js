71843b61e54621b6e87e7193e90ea31d6d921bfe
daryy01 <darlyapoloa@gmail.com>
2023-05-31 21:53:10 +0800

    Added joseph

--- renderer/js/renderer.js
+++ renderer/js/renderer.js
@@ -1,33 +1,52 @@
-// Btn Continue
-const btn_continue = document.getElementById("btn_continue");
-if (btn_continue) {
-  btn_continue.onclick = function (e) {
-    // Hide Intro Area and Show ChatBox
-    const intro_area = document.getElementById("intro-area");
-    const chat_area = document.getElementById("chat-area");
-    intro_area.classList.add('d-none');
-    chat_area.classList.remove('d-none');
-    chat_area.classList.add('d-flex');
-    // Show Latest Chat
-    const div_conversation = document.getElementById('div-conversation');
-    div_conversation.scrollTo(0, div_conversation.scrollHeight);
-    // Focus Input Type Message
-    const txt_message = document.getElementById('txt_message');
-    txt_message.focus();
-  }
-}
+const form = document.getElementById("bot_message");
 
-function alertMessages(status, message) {
+if (form) {
+  form.onsubmit = async function (e) {
+    e.preventDefault();//prevent the default form submission
+
+    const formData = new FormData(form);//creates new FormData object using form//easy access of forms input values
+    
+    //the input message is retrived from the form using formData.get("message");
+    let message = formData.get("message");
+    //checks if our message greater or equal to 5
+    if (message.length <= 5){
+      alertMessages( "error", "Input atleast a minimun of five characters!")
+      return;
+    }
+
+    //console.log(formData.get("message"));
+    //This function is responsible for making the API request to OpenAI and returning the response
+    const response = await window.axios.openAI(formData.get("message")); 
+    document.getElementById("message_response").innerHTML = JSON.stringify(response.choices[0].text).replace(/\\n/g, '');
+    
+    //Store to database the prompt and result
+const db_response = await window.axios.supaBase('post', '', {
+    message: message,
+    response: response
+    });
+    //console.log(db_response);
+  }
+};
+
+//responsible for displaying toast messages
+function alertMessages(status, message){
   window.Toastify.showToast({
     text: message,
     duration: 3000,
     stopOnFocus: true, // Prevents dismissing of toast on hover
     style: {
       textAlign: "center",
-      background: status == "error" ? "red" : "green",
+      background: status == "error" ? "red":"green", 
       color: "white",
       padding: "5px",
-      marginTop: "2px",
-    },
+      marginTop: "2px"
+    }
   });
 }
+ 
+// Get the elements
+document.getElementById("btn_continue").addEventListener("click", function() {
+  document.getElementById("intro-area").style.display = "none";
+  document.getElementById("chat-area").style.display = "block";
+});
+
